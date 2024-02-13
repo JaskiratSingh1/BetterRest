9481f075bb62f27ea4cdeba2956de8db6ccdd75d
Jaskirat Singh <jaskirat1999@gmail.com>
2024-02-12 21:35:09 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jaskirat Singh on 2024-02-10.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
