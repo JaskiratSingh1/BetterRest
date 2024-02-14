6e4b7a9dcff9e1ca4f05b7485af1bdfb66773a69
Jaskirat Singh <jaskirat1999@gmail.com>
2024-02-13 20:16:51 -0800

    Added bedtime computer var but live preview is broken so no time is shown. Must fix.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -12,10 +12,27 @@ struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    @State private var calculatedBedtime = ""
     
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var alertShowing = false
+    var bedTime: String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Int64(hour + minute), estimatedSleep: sleepAmount, coffee: Int64(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            calculatedBedtime = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            calculatedBedtime = "Sorry there was a problem calculating your bedtime"
+        }
+        return calculatedBedtime
+    }
     
     static var defaultWakeTime: Date {
         var components = DateComponents()
@@ -37,7 +54,6 @@ struct ContentView: View {
                 }
                 
                 Section("Daily coffee intake") {
-                    //Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 0...20)
                     Picker("^[\(coffeeAmount) cup](inflect: true)", selection: $coffeeAmount) {
                         ForEach(0..<11) {
                             Text("^[\($0) Cup](inflect: true)")
@@ -46,39 +62,14 @@ struct ContentView: View {
                     .labelsHidden()
                     .frame(maxWidth: .infinity, alignment: .center)
                 }
+                
+                Section("Recommended Bedtime") {
+                    Text("\(bedTime)")
+                    .font(.largeTitle)
+                }
             }
             .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-            }
-            .alert(alertTitle, isPresented: $alertShowing) {
-                Button("OK") {}
-            } message: {
-                Text(alertMessage)
-            }
         }
-    }
-    func calculateBedtime() {
-        do {
-            let config = MLModelConfiguration()
-            let model = try SleepCalculator(configuration: config)
-            
-            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-            let hour = (components.hour ?? 0) * 60 * 60
-            let minute = (components.minute ?? 0) * 60
-            
-            let prediction = try model.prediction(wake: Int64(hour + minute), estimatedSleep: sleepAmount, coffee: Int64(coffeeAmount))
-            
-            let sleepTime = wakeUp - prediction.actualSleep
-            
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-        } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry there was a problem calculating your bedtime"
-        }
-        
-        alertShowing = true
     }
 }
 
